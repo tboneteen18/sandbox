54d3207699ff5c12eb123d19bda12bc32244543a
Tboneteen18 <tboneteen18@gmail.com>
2015-06-04 11:58:18 -0600

    new push to proper repository test 2

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,11 @@
+class MealsController < ApplicationController
+
+  def index
+   @meals = Meal.all
+  end
+
+  def show
+    @meal = Meal.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
- root 'pages#home'
+
+  # /
+  root 'pages#home'
+
+  # /contact
+  get '/contact' => 'pages#contact'
+
+  # /about
+  get '/about' => 'pages#about'
+
+  # /meals
+  #get '/meals'=> 'meals#index'
+  resources :meals
 end
