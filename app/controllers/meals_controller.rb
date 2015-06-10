2ccd6bfd7d68f39160c905f02444cb364f5b0308
tboneteen18 <tboneteen18@gmail.com>
2015-06-10 13:47:26 -0600

    added bootstrap to the app

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,11 +1,51 @@
 class MealsController < ApplicationController
+  before_action :find_meal, only: [:edit, :update, :show, :destroy]
 
+  # GET /meals
   def index
    @meals = Meal.all
   end
 
   def show
-    @meal = Meal.find(params[:id])
+
   end
 
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+    if @meal.save
+      redirect_to meals_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @meal.update(meal_params)
+      redirect_to meals_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @meal.destroy
+    redirect_to meals_path
+  end
+
+  private
+  def meal_params
+    params.require(:meal).permit(:name,:calories,:restaurant,:preptime)
+  end
+
+  def find_meal
+    @meal = Meal.find(params[:id])
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def active_for_page(scope)
+    controller,action = *scope.split('#')
+    if params[:controller] == controller && params[:action] == action
+      return "active"
+    end
+  end
 end
